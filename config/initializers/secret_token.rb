d235bed9433a85c587bcad2a0e174c034ac47acf
Michael W. Miller <contact.mmiller@gmail.com>
2013-06-08 11:36:03 -0500

    Initial commit
    Build a blank pregres app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WAR::Application.config.secret_token = '891def008fb1f00f987d81e5e6a18b4d2714d6448d2f4b9d367637e49ab19a774f2f64ea194391a47973c5110fb3638f03423c33dc2bf9eb5ca02ec4185b72a1'
