d2e6bc38404992824f1f4f2b56136cd875e2cfc2
Michael W. Miller <contact.mmiller@gmail.com>
2013-06-09 23:33:17 -0500

    deck actions added
    new game button with action that creates a new deck, suffles and deals.

--- app/controllers/deck_controller.rb
+++ app/controllers/deck_controller.rb
@@ -1,3 +1,19 @@
 class DeckController < ApplicationController
+	layout 'standard'
+	def newGame
+		#build a fresh deck
+		@deck = Card.find(:all)
+		
+		#shuffle the deck
+		@deck = @deck.shuffle
+		 
+		#deal the deck
+		
+		@playerDeck = @deck.to(25)
+		@computerDeck = @deck.from(26)
+		
+		render 'home/battle'
+	end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -64,4 +64,5 @@ WAR::Application.routes.draw do
   #resources :home
   get "/home/battle/" => "home#battle"
   post "/home/battle/" => "home#battle"
+  get "/deck/new/" => "deck#newGame"
 end
