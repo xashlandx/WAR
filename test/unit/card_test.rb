35c1bcc67d396b34ed740d342b5bb6bb11e3418c
Michael W. Miller <contact.mmiller@gmail.com>
2013-06-28 22:06:46 -0500

    Unit testing added

--- test/unit/card_test.rb
+++ test/unit/card_test.rb
@@ -1,7 +1,62 @@
 require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  	#this tests to confirm that all cards have both a suit and a value
+	#this in effect also tests if there are any Jokers
+	#number of assertions = n x 2
+    def test_for_suits_and_values
+   		@testDeck = Card.find(:all) #build a fresh deck to test
+		
+		@testDeck.each do |testCard|
+			if testCard.suit != "Hearts" && testCard.suit != "Diamonds" && testCard.suit != "Clubs" && testCard.suit != "Spades"
+				assert false, "an incorrect suit is in the deck"
+			else
+				assert true
+			end
+			
+			if testCard.value >= 2 && testCard.value <= 14
+				assert true
+			else
+        		assert false, "an incorrect value is in the deck"
+      		end
+   		end #end loop
+	end #end test
+
+
+
+
+
+
+
+	#This is an alternate version for testing for suits and values.  This uses assert funtions
+	#written using the test macro syntax
+	#number of assertions = n x 2
+	test "another test for missing suit or value" do
+		@testDeck = Card.find(:all) #build a fresh deck to test
+	
+		@testDeck.each do |testCard|
+			assert_not_nil(testCard.suit, "there is a missing suit")
+			refute_nil(testCard.value, "there is a missing value")
+		end
+	end
+
+
+
+
+
+
+
+
+	#this tests specifically if any of the cards have the name "Joker"
+	#number of assertions = n
+    def test_for_jokers
+    	@testDeck = Card.find(:all)	 #build a fresh deck to test
+    	
+		@testDeck.each do |testCard|
+			assert_not_equal(testCard.name, "Joker", "a joker was found")
+		end
+	end
+ 
+   
 end
--- test/unit/deck_test.rb
+++ test/unit/deck_test.rb
@@ -1,7 +1,128 @@
 require 'test_helper'
 
 class DeckTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  
+  
+  
+  #This tests the deck size
+  #becuase the test data is small it always fails
+   test "deck size" do
+   	 #build a fresh deck to test
+	 @testDeck = Card.find(:all)
+     assert_equal 52, @testDeck.size, "the deck size does not equal 52"
+   end
+   
+   
+   
+   
+   
+   
+   #This tests that each card is unique
+   #This version is for short data sets n^2 number of assertions
+   #A larger data set would need a quick sort style recursion test
+   def test_cards_are_unique
+     #build a fresh deck to test
+	 @testDeck = Card.find(:all)
+	 
+	 $i=0
+	 while $i < @testDeck.count do
+		$k=0
+	 	while $k < @testDeck.count do
+			if @testDeck[$i].value == @testDeck[$k].value && @testDeck[$i].suit == @testDeck[$k].suit && $i!=$k
+				assert false, "A double card has been found"
+			else
+				assert true
+			end #end if
+			$k+=1
+		end #end inner loop
+		$i+=1
+	 end #end outer loop
+   end # end test_cards_are_unique
+   
+  
+  
+  
+   #This tests that each card is unique
+   #This version uses two bubble sorts to sort by value and suit before comparing
+   #This is less efficient but shows another option
+   #n-1 assertions
+   def test_cards_are_unique_bubble
+      
+	   #build a fresh deck to test
+	   @testDeck = Card.find(:all)
+		
+		#sort the suits
+		
+		#bubble sort the values	
+		@swapped=true
+		while @swapped != false
+			@swapped=false
+			@i=0
+			while @i < @testDeck.count-1 do
+				if @testDeck[@i].value > @testDeck[@i+1].value 
+					@buff = @testDeck[@i]
+					@testDeck[@i] = @testDeck[@i+1]
+					@testDeck[@i+1] = @buff
+					@swapped=true
+				end #end if
+				@i+=1
+			end # while
+		end # end outer while 
+		
+			
+		#sort by suit within each value
+		@i=0
+		while @i < @testDeck.count-1 do
+			#determine size of sub bubble
+			@groupCt=1
+
+			while @testDeck[@i].value == @testDeck[@i+@groupCt].value do
+				@groupCt+=1
+				if @i+@groupCt > @testDeck.count-1
+					break
+				end
+			end #end ct creation
+
+			#bubble sort the suits	
+			@swapped=true
+			while @swapped != false
+				@swapped=false
+				@k=0
+				
+				while @k < @groupCt-1 do
+					if (@testDeck[@i+@k].suit <=> @testDeck[@i+@k+1].suit) == 1
+						@buff = @testDeck[@i+@k]
+						@testDeck[@i+@k] = @testDeck[@i+@k+1]
+						@testDeck[@i+@k+1] = @buff
+						@swapped=true
+					end #end if
+					@k+=1
+				
+				end # while
+			end # end outer while
+			
+			@i+=@groupCt
+		end		
+	
+		#iterate through the sorted values and look for doubles
+		@i=0
+		while @i < @testDeck.count-1 do
+			if @testDeck[@i].value == @testDeck[@i+1].value && @testDeck[@i].suit == @testDeck[@i+1].suit
+				assert false, "A double card has been found"
+			else
+				assert true
+			end #end if
+			@i+=1
+		end
+	
+		#test output
+		#@testDeck.each do |testCard|
+			#puts testCard.value
+			#puts testCard.suit
+		#end
+		
+   end # test_cards_are_unique_bubble
+   
+    
 end
